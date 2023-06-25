6f9578b7b450d95e088f02583b26fc73e29d817d
Dzemil <dzemilpapic5@gmail.com>
2023-06-25 14:00:23 +0200

    check if place taken

--- xando.rb
+++ xando.rb
@@ -5,24 +5,37 @@ def show_board(board)
   puts board.map { |x| x.join(' ') }
 end
 
-board[0][1] = "X"
 show_board(board)
+
 class Player
   attr_accessor :name, :symbol
 
   def initialize(name, symbol)
     @name = name
     @symbol = symbol
-    @number = 0
+    @row = "."
+    @col = "."
   end
 
-  def play_turn(board, number)
-    unless number.between?(1,9)
-      puts "Please input number from 1 to 9"
+  def play_turn(board, row, col)
+    # check for valid input
+    unless row.between?(0,2)
+      unless col.between?(0,2)
+        return
+        puts "Please input a valid expression"
+      end
+    end
+
+    #check if board place taken
+    if board[row][col] != "."
+      puts "Place taken"
       return
     end
     
-    board[number] = symbol
-    puts board
+    board[row][col] = symbol
+    show_board(board)
   end
 end
+
+player1 = Player.new("Biljan", "X")
+player1.play_turn(board, 0, 0)
