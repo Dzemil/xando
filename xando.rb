f7613c9aa1cd82a98c299f409bd633130c475923
Dzemil <dzemilpapic5@gmail.com>
2023-07-02 11:24:51 +0200

    initialized game class

---
+++ xando.rb
@@ -0,0 +1,18 @@
+LINES = [[1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]]
+
+class Game
+  attr_reader :board, :current_player_id
+
+  def initialize(player_1_class, player_2_class)
+    @board = Array.new(10)
+
+    @current_player_id = 0
+    @players = [player_1_class.new(self, "X"), player_2_class.new(self, "O")]
+    puts "#{current_player} goes first."
+  end
+
+
+end
+
+class Player
+end
